3bdc6a2770f3fd51ecefa57f89c23a0b36b2366b
SlimBenAmara <36604339+SlimBenAmara@users.noreply.github.com>
2025-01-28 15:23:43 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 print('loading specific libraries')
 import pandas as pd
 print('Hello World')
-df1 = pandas.DataFrame({'a': [1, 3, 4], 'b': [5, 3, 1]}, columns = ['a', 'b'])
-df2 = pandas.DataFrame({'b': [5, 2, 0], 'a': [1, 2, 8]}, columns = ['b', 'a'], index = [2, 1, 0])
+df1 = pd.DataFrame({'a': [1, 3, 4], 'b': [5, 3, 1]}, columns = ['a', 'b'])
+df2 = pd.DataFrame({'b': [5, 2, 0], 'a': [1, 2, 8]}, columns = ['b', 'a'], index = [2, 1, 0])
 print(df1 + df2)
 
 print('Créer un dataframe échantillon')
