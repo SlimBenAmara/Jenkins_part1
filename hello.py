c32af8ce87c3c77549ada476eedaccd9a2a5ff38
SlimBenAmara <36604339+SlimBenAmara@users.noreply.github.com>
2025-01-28 18:05:19 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -11,16 +11,8 @@ pd.set_option('display.max_rows', 10)
 
 print('Create df1')
 df1 = pd.DataFrame({'a': [1, 3, 4], 'b': [5, 3, 1]}, columns = ['a', 'b'])
-print(df1)
-#df1_string = df1.to_csv(index=False)
-#os.environ['RESULT'] = df1_string  # Store in environment variable
-#df2 = pd.DataFrame({'b': [5, 2, 0], 'a': [1, 2, 8]}, columns = ['b', 'a'], index = [2, 1, 0])
-#print(df1 + df2)
+# Convert the DataFrame to CSV format (as a string)
+df_string = df1.to_csv(index=False)
 
-print('Créer un dataframe échantillon')
-data = {'A': [1, 2, 3, 4, 5], 'B': [5, 4, 3, 2, 1]}
-df = pd.DataFrame(data)
- 
-print('Créer un tracé de ligne')
-#df.plot()
-
+# Print the CSV string to verify it's correct (for debugging purposes)
+print("CSV string from df1:\n", df_string)
