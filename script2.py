9e7d7d31b77156a013762419487a35070781b5a3
SlimBenAmara <36604339+SlimBenAmara@users.noreply.github.com>
2025-01-28 17:34:53 +0100

    Update script2.py with OS

--- script2.py
+++ script2.py
@@ -1,6 +1,6 @@
 print('loading specific libraries')
 import pandas as pd
-
+import os
 
 print('settings')
 pd.set_option('display.notebook_repr_html', False)
@@ -8,9 +8,11 @@ pd.set_option('display.max_columns', 10)
 pd.set_option('display.max_rows', 10)
 
 
-print('Hello World')
-
+print('Create second dataframe : df2')
 df2 = pd.DataFrame({'b': [5, 2, 0], 'a': [1, 2, 8]}, columns = ['b', 'a'], index = [2, 1, 0])
+
+print('Call first dataframe : df1 using OS')
+df1 = os.getenv('RESULT')
 
 if 'df1' in locals() and 'df2' in locals():
     print(df1 + df2)
