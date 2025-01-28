71bfc18d55db4d88598d466f2f3cb6bd64d07082
SlimBenAmara <36604339+SlimBenAmara@users.noreply.github.com>
2025-01-28 15:22:53 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,14 @@
 print('loading specific libraries')
 import pandas as pd
 print('Hello World')
-d = {'col1': [1, 2], 'col2': [3, 4]}
-df = pd.DataFrame(data=d)
-print(df)
+df1 = pandas.DataFrame({'a': [1, 3, 4], 'b': [5, 3, 1]}, columns = ['a', 'b'])
+df2 = pandas.DataFrame({'b': [5, 2, 0], 'a': [1, 2, 8]}, columns = ['b', 'a'], index = [2, 1, 0])
+print(df1 + df2)
+
+print('Créer un dataframe échantillon')
+data = {'A': [1, 2, 3, 4, 5], 'B': [5, 4, 3, 2, 1]}
+df = pd.DataFrame(data)
+ 
+print('Créer un tracé de ligne')
+df.plot()
+
