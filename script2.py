a0dbb883a301aeaeaa3b62f208fb0a1261a9c175
SlimBenAmara <36604339+SlimBenAmara@users.noreply.github.com>
2025-01-28 18:02:11 +0100

    Update script2.py

--- script2.py
+++ script2.py
@@ -11,10 +11,9 @@ pd.set_option('display.max_rows', 10)
 print('Create second dataframe : df2')
 df2 = pd.DataFrame({'b': [5, 2, 0], 'a': [1, 2, 8]}, columns = ['b', 'a'], index = [2, 1, 0])
 
-print('Call first dataframe : df1 using OS')
-
+#print('Call first dataframe : df1 using OS')
 #df1 = pd.DataFrame({'a': [1, 3, 4], 'b': [5, 3, 1]}, columns = ['a', 'b'])
-print(df1)
+#print(df1)
 
 
 if 'df1' in locals() and 'df2' in locals():
