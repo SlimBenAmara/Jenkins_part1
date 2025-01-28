afc0e7705cba7cd71b8225064e1ffbf12138c312
SlimBenAmara <36604339+SlimBenAmara@users.noreply.github.com>
2025-01-28 15:29:03 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -10,8 +10,8 @@ pd.set_option('display.max_rows', 10)
 
 print('Hello World')
 df1 = pd.DataFrame({'a': [1, 3, 4], 'b': [5, 3, 1]}, columns = ['a', 'b'])
-df2 = pd.DataFrame({'b': [5, 2, 0], 'a': [1, 2, 8]}, columns = ['b', 'a'], index = [2, 1, 0])
-print(df1 + df2)
+#df2 = pd.DataFrame({'b': [5, 2, 0], 'a': [1, 2, 8]}, columns = ['b', 'a'], index = [2, 1, 0])
+#print(df1 + df2)
 
 print('Créer un dataframe échantillon')
 data = {'A': [1, 2, 3, 4, 5], 'B': [5, 4, 3, 2, 1]}
